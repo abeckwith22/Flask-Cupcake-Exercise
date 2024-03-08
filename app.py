f768bee57119e609b7d605f9e8b4a4f44efb21e9
abeckwith22 <140443409+abeckwith22@users.noreply.github.com>
2024-03-07 19:21:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Flask app for Cupcakes"""
-from flask import Flask, render_template, redirect, flash
-from models import connect_db, Cupcake 
+from flask import Flask, render_template, request, redirect, flash, jsonify
+from models import db, connect_db, Cupcake
 import requests
 
 def create_app():
@@ -12,7 +12,7 @@ def create_app():
     # app configurations
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
     app.config['SECRET_KEY'] = 'secret_key'
-    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = Fals    # app configurations
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False    # app configurations
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
     app.config['SECRET_KEY'] = 'secret_key'
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -21,27 +21,76 @@ def create_app():
 
     connect_db(app)
 
-    return appe
     app.config['SQLALCHEMY_ECHO'] = True
     app.config['DEBUG'] = True
-
-    connect_db(app)
 
     return app
 
 app = create_app()
 
+"""Flask Routes"""
+
 @app.route("/")
-def show_homepage():
+def display_homepage():
     return render_template('home.html')
 
 @app.route("/api/cupcakes")
-def show_homepage():
-    return render_template('home.html')
+def get_cupcakes():
+    """Gathers all cupcakes from db, displays them as JSON"""
+    cupcakes = Cupcake.query.all()
+    cupcake_serialized_list = [cupcake.serialize() for cupcake in cupcakes]
+    return jsonify(cupcake_serialized_list)
 
 @app.route("/api/cupcakes/<int:cupcake_id>")
-def show_homepage(cupcake_id):
-    return render_template('home.html')
+def get_specific_cupcake(cupcake_id):
+    cupcake = Cupcake.query.get(cupcake_id)
+    serialized = cupcake.serialize()
+    """Gets specific cupcake data from db using id, displays JSON"""
+    return jsonify(cupcake=serialized)
+
+@app.route("/api/cupcakes", methods=['POST'])
+def add_cupcake():
+    """Create cupcake from form data and return it."""
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+    new_cupcake = Cupcake(flavor=flavor, rating=rating, size=size, image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    # We should return a status code 201 for returned
+    return ( jsonify(serialized), 201 )
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=['PATCH'])
+def update_cupcake(cupcake_id):
+    """updates cupcake from db"""
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    cupcake.flavor = request.json.get('flavor', cupcake.flavor)
+    cupcake.size = request.json.get('size', cupcake.size)
+    cupcake.rating = request.json.get('rating', cupcake.rating)
+    cupcake.image = request.json.get('image', cupcake.image)
+
+    db.session.commit()
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=['DELETE'])
+def delete_cupcake(cupcake_id):
+    """Removes cupcake from db"""
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    return jsonify(message="deleted")
+
+
+""" Function Methods """
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, ssl_context=('cert.pem', 'key.pem'))
